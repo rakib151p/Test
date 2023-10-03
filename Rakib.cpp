32659b59db5f0db308174e1726fc0be02ca3a6dd
rakib151p <rakib151p@gmail.com>
2023-10-03 18:57:30 +0600

    Updated

---
+++ Rakib.cpp
@@ -0,0 +1,41 @@
+/*  Md Rakibul Islam
+    United International University
+    Dhaka,Bangladesh              */
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+#define ull long long
+#define uull unsigned long long int
+#define pb push_back
+#define p push
+#define pop pop_back
+#define pii pair<long long, long long>
+#define vp vector<pii>
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define loop0(j, n) for (int i = j; i < n; i++)
+#define sort(v) sort(v.begin(), v.end())
+#define sortrev(v) sort(v.begin(), v.end(), greater<int>())
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b)) * b
+typedef vector<long long> v;
+
+void solve()
+{
+    cout << "Rakib is pro, father of code";
+
+
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}   
